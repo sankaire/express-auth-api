8eb05984cd7a66ec893b094541156f959e73c378
Carver <kerenketepela@gmail.com>
2022-01-24 14:36:40 +0300

    set express midleware for parsing json

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const app = express()
 //import route
 const auth = require("./routes/auth")
 
-//route middleware
+//pull in db models
+require("./config/db")
+
+//middleware
 app.use("/api/user", auth)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
 const port = 3000
 app.listen(port, () => console.log(`app running on http://localhost:${port}`))
