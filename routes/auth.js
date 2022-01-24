150c4800c36e50839fc3d99342298eb13f981345
Carver <kerenketepela@gmail.com>
2022-01-24 14:35:29 +0300

    create new user

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,24 @@
 const router = require("express").Router()
+const User = require("../model/User")
 
 //register
-router.post('/register', (req, res) =>{
-    res.send("register")
+router.post('/register', async (req, res) =>{
+    const user = new User({
+        name:req.body.name,
+        email:req.body.email,
+        password:req.body.password
+    })
+    try{
+        const savedUser = await user.save()
+        res.send(savedUser)
+    } catch(err){
+        res.status(400).send(err)
+    }
 })
 
 //login
-router.post('/loggin', (req, res) =>{
+router.post('/login', (req, res) =>{
     res.send("login")
-})
+}) 
 
 module.exports = router
