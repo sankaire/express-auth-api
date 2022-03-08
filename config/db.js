7bf69aaef8a06b06cf68356b08bb993ea2293ba0
Carver <kerenketepela@gmail.com>
2022-03-08 22:53:33 +0300

    setup

--- config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-const dotenv = require("dotenv")
-
-dotenv.config()
-//connect to db
-mongoose.connect(
-    process.env.MONGO_URI,
-    () => console.log("connected to Database!!"))
-
--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-const express = require("express")
-const app = express()
-
-//import route
-const auth = require("./routes/auth")
-
-//pull in db models
-require("./config/db")
-
-//middleware
-app.use("/api/user", auth)
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
-
-const port = 3000
-app.listen(port, () => console.log(`app running on http://localhost:${port}`))
+// server
