7e1b2da9c4db9739f76624ec7b76a779ac55faeb
Carver <kerenketepela@gmail.com>
2022-03-09 18:12:22 +0300

    feat:initial server

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// server
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => console.log(`api running on ${PORT}`));
