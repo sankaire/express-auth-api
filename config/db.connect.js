ff65740bac843d3c9644c5f1547c0ec3813f1943
Carver <kerenketepela@gmail.com>
2022-03-09 19:55:46 +0300

    feat:connect to db

--- config/db.connect.js
+++ config/db.connect.js
@@ -1,1 +1,10 @@
-//db connection
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const url = process.env.MONGO_URI;
+
+mongoose
+  .connect(url)
+  .then(() => console.log('mongodb connected'))
+  .catch((err) => console.log(err));
