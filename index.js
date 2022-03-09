0592cc38a18905079144878f4a347ec187714ec9
Carver <kerenketepela@gmail.com>
2022-03-09 19:55:15 +0300

    feat: register routes on server

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const dotenv = require('dotenv');
+require('./config/db.connect');
 dotenv.config();
 
+const usersRouter = require('./routes/auth.route');
 //middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api/v1/users', usersRouter);
 
 const PORT = process.env.PORT;
 
