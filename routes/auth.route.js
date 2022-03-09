8e17a886f775ba50dc3aba1f63da0a5d824e663e
Carver <kerenketepela@gmail.com>
2022-03-09 19:54:36 +0300

    feat:create routes

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,1 +1,18 @@
-//routes
+const express = require('express');
+const router = express.Router();
+
+const {
+  createOne,
+  getAll,
+  updateOne,
+  removeOne,
+  getOne,
+} = require('../controllers/auth.controller');
+
+//api/v1/users
+router.route('/').post(createOne).get(getAll);
+
+//api/v1/users/:id
+router.route('/:id').put(updateOne).delete(removeOne).get(getOne);
+
+module.exports = router;
