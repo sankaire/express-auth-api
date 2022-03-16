1a92c88847e1dff198afbbb5140227dbac8b6f65
Carver <kerenketepela@gmail.com>
2022-03-16 18:47:38 +0300

    fix: use try catch

--- config/db.connect.js
+++ config/db.connect.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
 dotenv.config();
-const url = process.env.MONGO_URI;
 
-mongoose
-  .connect(url)
-  .then(() => console.log('mongodb connected'))
-  .catch((err) => console.log(err));
+const uri = process.env.MONGO_URI;
+try {
+  mongoose.connect(uri).then(console.log('mongodb connected'));
+} catch (err) {
+  console.log(err);
+}
